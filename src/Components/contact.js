89b2b216bb15d3ed6c06cce788037b56d0d9d3e4
vince gacayan <vincegacayan@gmail.com>
2021-03-29 12:49:33 +0800

    Update contact.js

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -6,6 +6,17 @@ class Contact extends Component{
             <div>
                 <h1>Contact Us</h1>
                 <p>This is Contact Page</p>
+                <form name="contact" netlify>
+                  <p>
+                    <label>Name <input type="text" name="name" /></label>
+                  </p>
+                  <p>
+                    <label>Email <input type="email" name="email" /></label>
+                  </p>
+                  <p>
+                    <button type="submit">Send</button>
+                  </p>
+                </form>
                 <div className="cont-bg"></div>
             </div>
         );
